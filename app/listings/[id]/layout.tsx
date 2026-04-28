33b3fb0ea5754598561a44a34f1daa96396be637
Trilokrana <ranatrilok1234@gmail.com>
2026-04-29 00:00:59 +0530

    delete: remove venue detail and booking request components

--- app/listings/[id]/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { GlobalSiteLayout } from "@/layout/GlobalSiteLayout"
-
-const layout = ({ children }: { children: React.ReactNode }) => {
-  return <GlobalSiteLayout>{children}</GlobalSiteLayout>
-}
-
-export default layout
